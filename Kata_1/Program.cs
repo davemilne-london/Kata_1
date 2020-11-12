1f8a79b4b267d43f5224677ddc33a3bf61466bec
dave_milneWB <dave.milne@webbeds.com>
2020-11-12 15:21:40 +0000

    Add project files.

---
+++ Kata_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kata_1_Code
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static string TranslateToRoman(uint Value)
+        {
+            var romanresult = "";
+
+            while (Value > 0)
+            {
+                romanresult += "I";
+                Value -= 1;
+            }
+
+            return romanresult;
+
+        }
+    }
+}
---
+++ Tests.Kata_1/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Kata_1_Code;
+
+namespace Kata_1_Tests
+{
+    public class Test
+    {
+
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("II", 2)]
+        [InlineData("III", 3)]
+        public void Return_RomanNumeral(string exp, uint input)
+        {
+
+            //Act
+            var actual = Program.TranslateToRoman(input);
+
+            //Assert
+            Assert.Equal(exp, actual);
+
+        }
+
+    }
+}
